5aac586aac6fe90a4a6985fec4d9a0191c8024d3
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-08-14 12:49:39 +0500

    add file

--- public/main.js
+++ public/main.js
@@ -7,6 +7,49 @@ mainNotFoundElement = document.querySelector(".main-not-found")
 
 mainAddElement.addEventListenter("click",event =>{
     event.preventDefault();
-    const tableElement = document.querySelector(".table")
-    tableElement.classList.remove("d-none")
+const  tableElement = document.querySelector(".table")
+    // tableElement.classList.add("d-block")
+
+    const tbodyElement = document.createElement("tbody")
+    const trElement = document.createElement("tr")
+    const newIdElement = document.createElement("td")
+    const newNameElement = document.createElement("td")
+    const newNumberElement = document.createElement("td")
+    const newKursElement = document.createElement("td")
+    const newKursTuriElement = document.createElement("td")
+    const newIshJoyiElement = document.createElement("td")
+    const newDarsVaqtiElement = document.createElement("td")
+    const newKelishManbasiElement = document.createElement("td")
+   
+    const asideKursElement = document.querySelector(".aside-kurs")
+    const asideTuriElement =  document.querySelector(".aside-turi")
+    const asideIshElement = document.querySelector(".aside-ish")
+    const asideVaqtiElement = document.querySelector(".aside-vaqti")
+    const asideManbaELement = document.querySelector(".aside-manba")
+
+    // shu jiyda ID qo'yish kerak
+    newNameElement.textContent = mainNameElement.value;
+    newNumberElement.textContent = mainTelElement.value;
+    newKursElement.textContent = asideKursElement.value;
+    newKursTuriElement.textContent =  asideTuriElement.value;
+    newIshJoyiElement.textContent =asideIshElement.value;
+    newDarsVaqtiElement.textContent =asideVaqtiElement.value;
+    newKelishManbasiElement.textContent =asideManbaELement.value;
+
+    trElement.appendChild(newNameElement)
+    trElement.appendChild(newNumberElement)
+    trElement.appendChild(newKursElement)
+    trElement.appendChild(newKursTuriElement)
+    trElement.appendChild(newIshJoyiElement)
+    trElement.appendChild(newDarsVaqtiElement)
+    trElement.appendChild(newKelishManbasiElement)
+
+ 
+    tbodyElement.prepend(trElement) 
+          
 })
+
+
+
+
+
